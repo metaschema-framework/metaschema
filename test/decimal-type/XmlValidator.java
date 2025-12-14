edb9c6c88fb65932f3c41791910760e6c1c8a65d
David Waltermire <davewaltermire@gmail.com>
2025-12-14 05:49:19 -0500

    Add file existence checks to XmlValidator for clearer error messages

--- test/decimal-type/XmlValidator.java
+++ test/decimal-type/XmlValidator.java
@@ -15,11 +15,23 @@ public class XmlValidator {
         String schemaPath = args[0];
         String xmlPath = args[1];
 
+        File schemaFile = new File(schemaPath);
+        File xmlFile = new File(xmlPath);
+
+        if (!schemaFile.exists()) {
+            System.out.println("Schema file not found: " + schemaPath);
+            System.exit(1);
+        }
+        if (!xmlFile.exists()) {
+            System.out.println("XML file not found: " + xmlPath);
+            System.exit(1);
+        }
+
         try {
             SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(new File(schemaPath));
+            Schema schema = factory.newSchema(schemaFile);
             Validator validator = schema.newValidator();
-            validator.validate(new StreamSource(new File(xmlPath)));
+            validator.validate(new StreamSource(xmlFile));
             System.out.println(xmlPath + " is VALID");
         } catch (Exception e) {
             System.out.println(xmlPath + " is INVALID: " + e.getMessage());
